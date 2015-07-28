a0a878cc99bb93c60e7f5b0c8d93ac45d17f4b02
John Doe <john@sample.com>
2015-07-28 23:41:11 +0300

    Added screenshots

--- LoginExample/infra/base_page.py
+++ LoginExample/infra/base_page.py
@@ -1,9 +1,12 @@
+# @PydevCodeAnalysisIgnore
 '''
 Created on Jul 9, 2015
 
 @author: Itai
 '''
 from infra.actionbot import ActionBot
+from allure.constants import AttachmentType
+import pytest
 
 class BasePageObject(object):
     '''
@@ -18,4 +21,8 @@ class BasePageObject(object):
         
         self.driver = driver
         self.bot = ActionBot(driver)
+        self.add_screenshot(self.__class__.__name__)
+        
+    def add_screenshot(self, description):
+        pytest.allure.attach(description, self.driver.get_screenshot_as_png(), type=AttachmentType.PNG)
         
